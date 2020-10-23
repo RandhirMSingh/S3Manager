9b712d43e7cb5fa5fbd3a68350d1065084052028
Randhir Singh <srandhir.334@gmail.com>
2020-10-23 22:37:07 +0530

    update script

--- setCredentials.sh
+++ setCredentials.sh
@@ -10,7 +10,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
+aws s3 cp ${FILE} s3://${S3_BUCKET}/${FILE} \
   --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
