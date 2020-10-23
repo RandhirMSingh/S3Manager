4f58fd7f25bd603c83de77a1fe8f6a5ad5b8f483
Randhir Singh <srandhir.334@gmail.com>
2020-10-23 22:47:30 +0530

    uploading content of dir

--- setCredentials.sh
+++ setCredentials.sh
@@ -10,8 +10,10 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${S3_BUCKET}/${FILE} \
-  --region ${AWS_REGION} $*
+#aws s3 cp ${FILE} s3://${S3_BUCKET}/ \
+ # --region ${AWS_REGION} $*
+  
+aws s3 cp ${FILE} s3://${S3_BUCKET}/ --recursive --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
 
