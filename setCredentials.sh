97f2ea99aa4ffb3e65e579ebf84f1814883c5540
Randhir Singh <srandhir.334@gmail.com>
2020-10-28 13:52:44 +0530

    adding repo and PR number as prefix to s3 objects

--- setCredentials.sh
+++ setCredentials.sh
@@ -13,7 +13,7 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 #aws s3 cp ${FILE} s3://${S3_BUCKET}/ \
  # --region ${AWS_REGION} $*
   
-aws s3 cp ${FILE} s3://${S3_BUCKET}/ --recursive --region ${AWS_REGION} $*
+aws s3 cp ${FILE} s3://${S3_BUCKET}/${REPO}/${PR_NUMBER} --recursive --region ${AWS_REGION} $*
 ls -all
 #this script downloads images from s3 bucket and updates PR
 swift /downlaod_S3Object.swift ${S3_BUCKET} ${AWS_REGION} ${REPO} ${PR_NUMBER} ${GITHUB_TOKEN}
