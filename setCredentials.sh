22d70c0d3d69c2f4ec86ba3634647f1dd573b358
Randhir Singh <srandhir.334@gmail.com>
2020-10-29 10:32:28 +0530

    Update setCredentials.sh

--- setCredentials.sh
+++ setCredentials.sh
@@ -14,7 +14,9 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
  # --region ${AWS_REGION} $*
   
 aws s3 cp ${FILE} s3://${S3_BUCKET}/${REPO}/${PR_NUMBER} --recursive --region ${AWS_REGION} $*
-ls -all
+echo PR_Number: ${PR_NUMBER}
+echo repo Name: ${REPO}
+
 #this script downloads images from s3 bucket and updates PR
 swift /downlaod_S3Object.swift ${S3_BUCKET} ${AWS_REGION} ${REPO} ${PR_NUMBER} ${GITHUB_TOKEN}
 
